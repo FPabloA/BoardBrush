ecc1d092ec1447fedecef16f5d70a0e66153c707
FPabloA <pabloayala@protonmail.com>
2023-05-31 13:08:58 -0700

    imported css styling from old version and setup username display

--- src/loadpage/load.js
+++ src/loadpage/load.js
@@ -1,7 +1,46 @@
+import "./load.css";
+
 function Load({loggedUser}) {
-    console.log(loggedUser);
-    return(<>
-    <div>load page</div>
-    </>)
+    const parseUser = () => {
+        const ind = loggedUser.email.indexOf("@");
+        return loggedUser.email.substring(0, ind);
+    }
+
+    return (
+        <>
+        <div className="wrapper">
+        <div className="load-header">
+            
+            <span className="load-header-text">Load Boards</span>
+            <span className="load-header-name">{parseUser()}</span>
+        </div>
+            <div className="load-recent">
+                <div className="load-recent-header">
+                    <span className="load-recent-text">Recent Boards</span>
+                    <button className="load-recent-button" >
+                        <span className="load-new-text">New Board</span> 
+                        
+                    </button>
+                </div>
+            </div>
+            <div className="load-folders">
+                <div className="load-folder-header">
+                    <span className="load-recent-text">Folders</span>
+                    <button className="load-recent-button">
+                        <span className="load-new-text">Add Folder</span> 
+                        
+                    </button>
+                </div>
+            </div>
+            <div className="load-footer">
+                <button className="load-exit-button" >
+                    <span className="load-exit-text">Exit</span>
+                    
+                </button>
+            </div>
+        </div>
+            
+        </>
+    );
 }
 export default Load;
